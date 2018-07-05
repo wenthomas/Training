49dfc434c6b9048c3cf6d67b32759782289603e7
MarkChburg <31096878+MarkChberg@users.noreply.github.com>
2018-07-06 06:32:23 +0800

    指出了小问题，回答了问题

--- src/stacktest/ArrayToStack.java
+++ src/stacktest/ArrayToStack.java
@@ -3,10 +3,10 @@ package stacktest;
 public class ArrayToStack {
 
     private int[] stack;
-    int top;    //栈顶指针
+    int top;    //栈顶指针，todo：直接在这里赋初始值就可以了
 
     public ArrayToStack(){
-
+        // todo：无参构造器也要就是初始化的
     }
     public ArrayToStack(int a){
         this.stack = new int[a];
@@ -24,7 +24,7 @@ public class ArrayToStack {
     public void push(int a){
         if(!isFull()){
             this.stack[++this.top] = a;
-            printAll();
+            printAll(); // 此方法对于栈是没有必要的，栈对外暴露的是最上面的那个
         }
     }
     public int pop(){
@@ -39,7 +39,7 @@ public class ArrayToStack {
     public int peek(int index){
         if(index < 0 || index > this.top){
             System.out.println("索引有误！");
-            return 0;   //问：如何完善此处返回内容？
+            return 0;   //问：如何完善此处返回内容？ todo：诸如此类都抛出异常
         }else{
             System.out.println("返回元素：" + this.stack[index]);
             return this.stack[index];
